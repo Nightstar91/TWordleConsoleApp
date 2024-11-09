df0e10ea6e09db70b76f9807127469c91cd6f6aa
Alvaro <Alvaro@DESKTOP-9AI2NRC>
2024-11-08 21:44:41 -0600

    Add project files.

---
+++ TWordleConsoleApp/Library.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TWordleConsoleApp
+{
+    public class Library
+    {
+        public static void WriteMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+
+        public static void WriteMessage(string message, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(message);
+            Console.ResetColor();
+        }
+
+
+        public static void Pause()
+        {
+            Console.WriteLine("Press the ENTER key to continue...");
+            Console.ReadKey();
+        }
+
+
+        public static void ClearConsole()
+        {
+            Console.Clear();
+        }
+    }
+}
---
+++ TWordleConsoleApp/Menu.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static TWordleConsoleApp.Library;
+
+namespace TWordleConsoleApp
+{
+    public class Menu
+    {
+        public string userInput;
+        private bool nameValidation;
+
+        public void StartScreen()
+        {
+            userInput = "";
+            nameValidation = false;
+
+            while(nameValidation != true)
+            {
+                // Prompt the user
+                WriteMessage("Please enter a name ");
+
+                // Storing what the user typed
+                userInput = Console.ReadLine();
+
+                if (userInput != "")
+                {
+                    // Prompt the user they enter a valid response
+                    WriteMessage($"Hello there {userInput}, Welcome to Wordle!");
+                    Pause();
+                    nameValidation = true;
+
+                }
+                else
+                {
+                    WriteMessage("Please enter something into the prompt!");
+                    Pause();
+                }
+            }
+
+            
+        }
+    }
+}
